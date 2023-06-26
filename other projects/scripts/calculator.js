9206bd2894f0c4cad8480275c3584cf794bc4d9f
dv3rgntfr33lncr <dv3rgntfr33lncr@aol.com>
2023-06-25 19:16:51 -0600

    Add files via upload

---
+++ other projects/scripts/calculator.js
@@ -0,0 +1,20 @@
+﻿function calculatorDisplay(display, value) {
+    document.getElementById("display").value += value;
+}
+
+function calculatorClear(display, value) {
+    document.getElementById("display").value = '';
+}
+
+function calculatorClearEntry(display, value) {
+    var len = document.getElementById("display").value.length;
+    document.getElementById("display").value = document.getElementById("display").value.substr(0, len-1);
+}
+
+function result(form) {
+    var result;
+
+    result = document.getElementById("display").value;
+
+    document.getElementById("display").value = eval(result);   
+}
